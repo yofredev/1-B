7b74ea710c407de93123c655e3b6942f7ecfc687
yofredev <164373885+yofredev@users.noreply.github.com>
2024-04-10 19:52:54 -0400

    Add files via upload

---
+++ assest/app.js
@@ -0,0 +1,36 @@
+const openMenu = document.querySelector(".menu-open"),
+    navMenu = document.querySelector(".menu"),
+    closeMenu = document.querySelector(".menu-close")
+
+if(openMenu){
+    openMenu.addEventListener('click',() =>{
+        navMenu.classList.add("show")
+    })
+}
+if(closeMenu){
+    closeMenu.addEventListener('click', ()=>{
+        navMenu.classList.remove("show")
+    })
+
+}
+
+const toggle= document.getElementById("toggle"),
+      root = document.querySelector(":root")
+      toggle.addEventListener('change',(event)=>{
+            let checked = event.target.checked; 
+        if(checked == true){
+           root.style.setProperty('--background-color-menu' , '#151515')
+           root.style.setProperty('--menu-font-color' , '#f7f7f7')
+           root.style.setProperty('--background-color','#151515')
+           root.style.setProperty('--font-color','#F7f7f7')
+        }
+        else{
+            root.style.setProperty('--background-color-menu' , '#f7f7f7')
+            root.style.setProperty('--menu-font-color' , '#151515')
+            root.style.setProperty('--background-color','#f7f7f7')
+            root.style.setProperty('--font-color','#f7f7f7')
+        }
+
+    })
+
+
